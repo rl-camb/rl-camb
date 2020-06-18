fc7423fad9323e07e1cfbec12635957e2865c81a
James Bernardi <james.bernardi@audioanalytic.com>
2020-06-18 19:28:00 +0100

    Fixing test saving dir and adding todos for action test

--- james_workspace/spinning_up/tests/test_model_runs.py
+++ james_workspace/spinning_up/tests/test_model_runs.py
@@ -4,6 +4,10 @@ import tempfile
 from env import CartPoleStandUp
 from models import DQNSolver, VPGSolver, VPGSolverWithMemory
 
+agents = (DQNSolver, VPGSolver, VPGSolverWithMemory)
+
+
+# TODO - add testing that clone(model).save model = load is the same (e.g. saves and loads)
 class TestModelsRun(unittest.TestCase):
 
     env = CartPoleStandUp(
@@ -11,7 +15,10 @@ class TestModelsRun(unittest.TestCase):
 
     outdir = tempfile.TemporaryDirectory()
 
-    std_agent_args = ("test", env.observation_space, env.action_space)
+    std_agent_args = (
+        outdir + "/test",
+        env.observation_space,
+        env.action_space)
 
     def test_dqn_train(self):
         agent = DQNSolver(*self.std_agent_args)
@@ -36,3 +43,24 @@ class TestModelsRun(unittest.TestCase):
     def test_vpg_with_memory_show(self):
         agent = VPGSolverWithMemory(*self.std_agent_args)
         agent.show(self.env)
+
+
+# TODO - complete
+class TestAct(unittest.TestCase):
+
+    env = CartPoleStandUp(
+        score_target=195., episodes_threshold=100, reward_on_fail=-10.)
+
+    def test_act_random(self):
+        # test epsilon == 1 for agent in agents
+        # state = env.reset, use state as the input
+        pass
+
+    def test_act_determine(self):
+        # test epsilon == None
+        pass
+
+    def test_act_fails(self):
+        # test epsilon < 0 or > 1 and test 
+        # Also test a badly shaped state, like a batch or not matching action space
+        pass
